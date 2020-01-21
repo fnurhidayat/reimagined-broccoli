ae77c85ee0ed1383837f0311f67e22d52babe80c
firbert96 <firbert96@gmail.com>
2020-01-21 11:21:21 +0700

    [firb] update user

---
+++ update.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+function updateUser(id,obj){
+    let objJSON = require('./data/users.json');
+    let objJSONMap = objJSON.map(i=>{
+        if(i.id===id){
+            return obj;
+        }
+        else{
+            return i;
+        }
+    });
+    fs.writeFileSync(
+        `./data/users.json`,
+        JSON.stringify(objJSONMap,null,2)
+    ); 
+}
