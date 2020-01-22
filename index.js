ebf4ee750ad097647e26ad6c3ff09300f9ef613d
afdallah <afdallah.war@gmail.com>
2020-01-22 20:55:34 +0700

    [Dallah] Refactor using Promise

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ const method = args[0]
 switch(method) {
   case 'create_users':
     const [ name, email, password, password_confirmation ] = args.slice(1);
-    create({ name, email, password, password_confirmation });
+    create.user({ name, email, password, password_confirmation })
+      .then(data => console.log(data))
+      .catch(err => console.error(err))
     break;
   case 'update_users':
     let id = args[1];
     let obj = JSON.parse(args[2]);
     update(id,obj);
+    break;
+  case 'create_posts':
+    const [ title, body ] = args.slice(1)
+    create.post({ title, body })
+      .then(data => console.log(data))
+      .catch(err => console.error(err))
     break;
   default:
     console.log('Unknown operation!')
