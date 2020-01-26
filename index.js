3447263d7051ddd8800339a6e9da91d8dcb4715d
afdallah <afdallah.war@gmail.com>
2020-01-26 22:36:23 +0700

    [Dallah] Implement create user route using express

--- index.js
+++ index.js
@@ -1,79 +1,101 @@
-const create = require('./libs/create.js');
-const updateUser = require('./libs/update.js').user;
-const deleteUser = require('./libs/delete.js').user;
-const updatePost = require('./libs/update.js').post;
-const deletePost = require('./libs/delete.js').post;
-const read = require('./libs/read.js');
-
 const User = require('./models/user');
 const Post = require('./models/post');
 const args = process.argv.slice(2);
 const method = args[0];
 
-switch (method) {
-  case 'create_users':
-    const [name, email, password, password_confirmation] = args.slice(1);
-    const user = new User({
-      name,
-      email,
-      password,
-      password_confirmation,
-    });
+const express = require('express');
+const app = express();
+const port = 3000;
 
-    user
-      .save()
-      .then(data => console.log(data))
-      .catch(err => console.error(err));
+app.use(express.json());
 
-      // user
-      // .create()
-      // .then(data => console.log(data))
-      // .catch(err => console.error(err));
-        break;
-    case 'update_users':
-        let id_updateUser = args[1];
-        let objUser = JSON.parse(args[2]);
-        const user_update = new User(objUser);
-        user_update.update(id_updateUser).then(data=>{
-            console.log(data);
-        });
-        break;
-    case "delete_users":
-        let id_deleteUser = args[1];
-        const user_delete = new User();
-        user_delete.delete(id_deleteUser).then(data => {
-            console.log(data);
-        });
-        break;
-    case "update_post":
-        let id_updatePost = args[1];
-        let objPost = JSON.parse(args[2]);
-        const post_update = new Post(objPost);
-        post_update.update(id_updatePost).then(data=>{
-            console.log(data);
-        });
-        break;
-    case "delete_post":
-        let id_deletePost = args[1];
-        const post_delete = new Post();
-        post_delete.delete(id_deletePost).then(data => {
-            console.log(data);
-        }).catch(error => {
-            console.log(error);
-        });
-        break;
-    case "read_user":
-        read(args.slice(1)[0]);
-        break;
-    case "create_posts":
-        const [title, body] = args.slice(1);
-        const post = new Post({ title, body });
+// CREATE
+app.post('/users', async function(req, res) {
+  try {
+    const data = await new User(req.body).save()
 
-    post
-      .create()
-      .then(data => console.log(data))
-      .catch(err => console.error(err));
-    break;
-  default:
-    console.log('Unknown operation!');
-}
+    res
+      .status(201)
+      .json({
+        status: true,
+        data
+      });
+  } catch (err) {
+    res
+      .status(400)
+      .json({
+        status: false,
+        error: err
+      })
+  }
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+// switch (method) {
+//   case 'create_users':
+//     const [name, email, password, password_confirmation] = args.slice(1);
+//     const user = new User({
+//       name,
+//       email,
+//       password,
+//       password_confirmation,
+//     });
+
+//     user
+//       .save()
+//       .then(data => console.log(data))
+//       .catch(err => console.error(err));
+
+//       // user
+//       // .create()
+//       // .then(data => console.log(data))
+//       // .catch(err => console.error(err));
+//         break;
+//     case 'update_users':
+//         let id_updateUser = args[1];
+//         let objUser = JSON.parse(args[2]);
+//         const user_update = new User(objUser);
+//         user_update.update(id_updateUser).then(data=>{
+//             console.log(data);
+//         });
+//         break;
+//     case "delete_users":
+//         let id_deleteUser = args[1];
+//         const user_delete = new User();
+//         user_delete.delete(id_deleteUser).then(data => {
+//             console.log(data);
+//         });
+//         break;
+//     case "update_post":
+//         let id_updatePost = args[1];
+//         let objPost = JSON.parse(args[2]);
+//         const post_update = new Post(objPost);
+//         post_update.update(id_updatePost).then(data=>{
+//             console.log(data);
+//         });
+//         break;
+//     case "delete_post":
+//         let id_deletePost = args[1];
+//         const post_delete = new Post();
+//         post_delete.delete(id_deletePost).then(data => {
+//             console.log(data);
+//         }).catch(error => {
+//             console.log(error);
+//         });
+//         break;
+//     case "read_user":
+//         read(args.slice(1)[0]);
+//         break;
+//     case "create_posts":
+//         const [title, body] = args.slice(1);
+//         const post = new Post({ title, body });
+
+//     post
+//       .create()
+//       .then(data => console.log(data))
+//       .catch(err => console.error(err));
+//     break;
+//   default:
+//     console.log('Unknown operation!');
+// }
